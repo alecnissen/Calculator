bbb72b1cd7c1cc8f72fac46f890dc77957153ce9
alecnissen <alecnissen@yahoo.com>
2022-09-06 13:57:13 -0400

    created the 4 operation functions, tested and all are working

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 // create functions 
-
-// function add(nums) { 
-//     let sum = 0; 
-
-//     for (let i = 0; i < nums.length; i++) { 
-//         let num = nums[i]; 
-
-//         sum += num; 
-//     }
-//     return sum; 
-// } 
  
-// how to create a function that adds an unlimited amount of numbers  
 
 function add(...nums) { 
     let sum = 0; 
@@ -28,7 +16,38 @@ function subtract(...nums) {
 
     for (let i = 0; i < nums.length; i++) { 
         let num = nums[i]; 
-        total -= num; 
+        total =- num; 
     }
     return total; 
-}
+} 
+
+
+function multiply(...nums) { 
+    let product = 1; 
+
+    for (let i = 0; i < nums.length; i++) { 
+        let num = nums[i]; 
+
+        product *= num; 
+
+    }
+    return product; 
+} 
+
+function divide(input1,... input2) { 
+    let quotient = input1 / input2; 
+    return quotient; 
+}  
+
+// function divide(input1,... input2) { 
+
+// let result = 0; 
+
+// s1 = parseInt(input1); 
+// s2 = parseInt(input2); 
+
+// if (s2!=0) { 
+//     result = s1 / s2; 
+// }
+//     return result; 
+// } 
